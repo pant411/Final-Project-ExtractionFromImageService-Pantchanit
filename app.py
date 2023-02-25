0ce52746a1747ac9bfe840b3a187c96570c6208a
pant411 <pantchanit2561@hotmail.com>
2023-02-26 00:59:00 +0700

    initial

---
+++ app.py
@@ -0,0 +1,46 @@
+# built in module
+from typing import List, Union
+from fastapi import FastAPI, File, UploadFile, Form
+from fastapi.middleware.cors import CORSMiddleware
+# from werkzeug.utils import secure_filename
+import cv2
+import numpy as np
+
+# my module
+from mainModule import runMain # main application of project
+
+
+app = FastAPI(title="My Final Project", debug=True)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/")
+def home():
+    return "Hello World!!!!"
+
+@app.post("/receipts/uploadfile/submitreceipt", tags = ["Receipts"])
+async def submitReceipt1(type_receipt: int = Form(...), 
+                         file: UploadFile = File(...)):
+    content_receipt = file.file.read()
+    # print(content_receipt)
+    image = cv2.imdecode(np.fromstring(content_receipt, np.uint8),\
+            cv2.IMREAD_UNCHANGED)      
+    data = runMain(image, type_receipt)
+    return data
+
+@app.post("/receipts/file/submitreceipt", tags = ["Receipts"])
+async def submitReceipt2(type_receipt: int = Form(...), 
+                         file: bytes = File(...)):
+    # print(content_receipt)
+    image = cv2.imdecode(np.fromstring(file, np.uint8),\
+            cv2.IMREAD_UNCHANGED)      
+    data = runMain(image, type_receipt)
+    return data
